8ad7443b5f78e003b7bdf4c870304a893831ff46
Aleksey Logachev <a.logachev@crpo.su>
2025-05-19 11:51:50 +0300

    1-start Added a user info getting function and a blank currency conversion function.

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,10 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	moneyAmount, initialCurrency, targetCurrency := getUserInput()
+
+	msg := fmt.Sprintf("You want to convert %.0f %s to %s", moneyAmount, initialCurrency, targetCurrency)
+	fmt.Println(msg)
+
 	const usdToEur = 0.9
 	const usdToRub = 81.07
-
 	eurToRub := (1 / usdToEur) * usdToRub
 
 	fmt.Println(eurToRub)
+}
+
+func getUserInput() (float64, string, string) {
+	var moneyAmount float64
+	var initialCurrency, targetCurrency string
+
+	fmt.Println("__Welcome to the currency converter__")
+	fmt.Print("Set an amount of money you want to convert: ")
+	fmt.Scan(&moneyAmount)
+	fmt.Print("Set a currency you want to convert from: ")
+	fmt.Scan(&initialCurrency)
+	fmt.Print("Set a currency you want to convert to: ")
+	fmt.Scan(&targetCurrency)
+
+	return moneyAmount, initialCurrency, targetCurrency
+}
+
+func convertAmount(amount float64, initialCurrency string, targetCurrency string) float64 {
 }
