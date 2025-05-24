37f6b80893aff94c2d0e4b68020b51ca030c2630
Aleksey Logachev <a.logachev@crpo.su>
2025-05-24 12:31:37 +0300

    2-calc Added user input.

--- 2-calc/main.go
+++ 2-calc/main.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+const AVG, SUM, MED = "AVG", "SUM", "MED"
+
+func main() {
+	// Принимаем строку из чисел через запятую ("2,3,10,-6"), их нужно разбить по запятой, удалить лищние пробелы
+	// В итоге получаем слайс с числами, которые были переданы через запятую
+	operation, integers := getUserInput()
+	fmt.Println(operation, integers)
+}
+
+func getUserInput() (string, string) {
+	operation := getOperationInput()
+	integers := getIntegersInput()
+	msg := fmt.Sprintf("You want to make %s operation with integers %s", operation, integers)
+	fmt.Println(msg)
+
+	return operation, integers
+}
+
+func getOperationInput() string {
+	var operation string
+
+	for {
+		fmt.Print("Enter operation (AVG|SUM|MED): ")
+		fmt.Scan(&operation)
+
+		if operation == AVG || operation == SUM || operation == MED {
+			break
+		}
+		fmt.Println("Wrong operation input. Try again.")
+	}
+
+	return operation
+}
+
+func getIntegersInput() string {
+	var integers string
+	var integer string
+
+	for {
+		fmt.Print("Enter integers (to finish input, enter not integer symbol, empty input is not allowed): ")
+		fmt.Scan(&integer)
+		_, err := strconv.Atoi(integer)
+
+		if err != nil && integers != "" {
+			break
+		} else if err != nil && integers == "" {
+			fmt.Println("Empty input is not allowed. Try again.")
+			continue
+		}
+
+		if integers == "" {
+			integers = integer
+		} else {
+			integers += ", " + integer
+		}
+	}
+
+	return integers
 }
