40d922772b822c9384450d932dfca2ec516e0a78
Aleksey Logachev <a.logachev@crpo.su>
2025-06-10 21:15:52 +0300

    3-bin Added creation methods.

--- 3-bin/bins/bins.go
+++ 3-bin/bins/bins.go
@@ -1,14 +1,39 @@
 package bins
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
+	Id        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
 }
 
 type BinList struct {
-	bins []Bin
+	Bins []Bin
+}
+
+func NewBin(id, name string, private bool) (*Bin, error) {
+	if id == "" {
+		return nil, errors.New("Empty ID")
+	}
+	if name == "" {
+		return nil, errors.New("Empty name")
+	}
+
+	newBin := Bin{
+		Id:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+
+	return &newBin, nil
+}
+
+func NewBinList(bins []Bin) *BinList {
+	return &BinList{Bins: bins}
 }
