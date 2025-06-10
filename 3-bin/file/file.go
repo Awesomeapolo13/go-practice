9ab00ddea4486ab801380f76f1d35a7d1bf81326
Aleksey Logachev <a.logachev@crpo.su>
2025-06-10 21:15:32 +0300

    3-bin Added methods to work with files.

--- 3-bin/file/file.go
+++ 3-bin/file/file.go
@@ -1,1 +1,42 @@
 package file
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func ReadFile(name string) ([]byte, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func IsJSON(fileName string) bool {
+	var js map[string]interface{}
+
+	return json.Unmarshal([]byte(fileName), &js) == nil
+}
+
+func WriteFile(content []byte, name string) {
+	if !IsJSON(name) {
+		fmt.Println(name + " not json file")
+	}
+
+	file, err := os.Create(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(content)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Wrote successfully")
+}
