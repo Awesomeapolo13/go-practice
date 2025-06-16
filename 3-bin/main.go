b4dfceb64f1ce851ec9161841c9d2c43febdebfc
Aleksey Logachev <a.logachev@crpo.su>
2025-06-16 18:11:28 +0300

    4-interface Added interfaces.

--- 3-bin/main.go
+++ 3-bin/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bin/bins"
+)
+
+type StorageInterface interface {
+	AddBin(bin bins.Bin)
+	FindAllBins() *bins.BinList
+	ToByteSlice() ([]byte, error)
+}
+
 func main() {
 
 }
