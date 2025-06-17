1d7cc55f0abda30f8e74a55f4fbc5ddbd1ac139c
Aleksey Logachev <a.logachev@crpo.su>
2025-06-17 21:03:19 +0300

    4-interface Added api with stubs.

--- 3-bin/api/api.go
+++ 3-bin/api/api.go
@@ -1,7 +1,25 @@
 package api
 
-import "bin/config"
+import (
+	"bin/bins"
+	"bin/config"
+)
 
 type API struct {
 	config config.Config
+}
+
+func NewAPI(config config.Config) *API {
+	return &API{
+		config: config,
+	}
+}
+
+func (api *API) LoadToCloud(bins bins.BinList) {
+	// Пока что заглушка
+}
+
+func (api *API) LoadFromCloud() bins.BinList {
+	// Пока что заглушка
+	return bins.BinList{}
 }
