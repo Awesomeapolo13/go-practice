325da4dd5b183923ebc72fb122e2093f1bd5627e
Aleksey Logachev <a.logachev@crpo.su>
2025-06-24 15:17:10 +0300

    5-final Modified create bin method and realized update method.

--- 3-bin/api/api.go
+++ 3-bin/api/api.go
@@ -26,6 +26,7 @@ type ConfigInterface interface {
 
 type StorageInterface interface {
 	AddBin(bin bins.Bin)
+	FindBinById(id string) (*bins.Bin, error)
 	FindAllBins() *bins.BinList
 	ToByteSlice() ([]byte, error)
 }
@@ -48,44 +49,42 @@ func (api *API) GetBin(id string) {
 	// Заглушка
 }
 
-func (api *API) CreateBin(binName string, data []byte) (bins.Bin, error) {
+func (api *API) CreateBin(binName string, data []byte) (*bins.Bin, error) {
 	url := apiRootUrl + "/b"
-	req, err := http.NewRequest(POST, url, bytes.NewBuffer(data))
+	req, err := api.prepareRequest(url, POST, data)
 	if err != nil {
-		panic("Failed to create request: " + err.Error())
+		return nil, err
 	}
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("X-Master-Key", api.config.GetMasterKey())
-	req.Header.Add("X-Access-Key", api.config.GetAccessKey())
 	req.Header.Add("X-Bin-Name", binName)
 
-	resp, err := api.client.Do(req)
+	body, err := api.getResponse(url, req)
 	if err != nil {
-		fmt.Println("Failed request " + url + ": " + err.Error())
-		return bins.Bin{}, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != allowedStatusCode {
-		return bins.Bin{}, errors.New("Wrong response status: " + resp.Status)
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return bins.Bin{}, errors.New("Failed to read response body: " + err.Error())
+		return nil, err
 	}
 
 	var bin bins.Bin
 	err = json.Unmarshal(body, &bin)
 	if err != nil {
-		return bins.Bin{}, errors.New("Failed to unmarshal response body: " + err.Error())
+		return nil, errors.New("Failed to unmarshal response body: " + err.Error())
 	}
 	bin.Name = binName
 	api.storage.AddBin(bin)
 
-	return bin, nil
+	return &bin, nil
 }
 
-func (api *API) UpdateBin(bin bins.Bin) {
-	// Заглушка
+func (api *API) UpdateBin(binId string, data []byte) error {
+	url := apiRootUrl + "/b/" + binId
+	req, err := api.prepareRequest(url, PUT, data)
+	if err != nil {
+		return err
+	}
+	_, err = api.getResponse(url, req)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (api *API) DeleteBin(bin bins.Bin) {
@@ -103,4 +102,34 @@ func (api *API) LoadFromCloud() bins.BinList {
 
 func (api *API) addToStorage(bins.Bin) {
 
+}
+
+func (api *API) prepareRequest(url, method string, data []byte) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, errors.New("Failed to create request: " + err.Error())
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-Master-Key", api.config.GetMasterKey())
+	req.Header.Add("X-Access-Key", api.config.GetAccessKey())
+
+	return req, nil
+}
+
+func (api *API) getResponse(url string, req *http.Request) ([]byte, error) {
+	resp, err := api.client.Do(req)
+	if err != nil {
+		fmt.Println("Failed request " + url + ": " + err.Error())
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != allowedStatusCode {
+		return nil, errors.New("Wrong response status: " + resp.Status)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("Failed to read response body: " + err.Error())
+	}
+
+	return body, nil
 }
