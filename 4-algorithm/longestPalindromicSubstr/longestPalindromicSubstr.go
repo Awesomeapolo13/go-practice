47f2d6f76aead53ddcf7aecacd814c0eb534844c
Aleksey Logachev <a.logachev@crpo.su>
2025-07-27 21:16:22 +0300

    algorithm-2 Added palindromic longest substr simple algorithm.

---
+++ 4-algorithm/longestPalindromicSubstr/longestPalindromicSubstr.go
@@ -0,0 +1,53 @@
+package longestPalindromicSubstr
+
+func FindLongestPalindrome(s string) string {
+	var maxLength int
+	str := s
+	includeLastSymbol := true
+	length := len(s)
+	i, j := 0, length-1
+	if length == 1 {
+		return str
+	}
+
+	palindromes := map[int]string{}
+	for i < length {
+		if isPalindrome(str) {
+			substrLength := len(str)
+			id, _ := palindromes[substrLength]
+			if id == "" && substrLength > maxLength {
+				maxLength = substrLength
+			}
+			if id == "" {
+				palindromes[substrLength] = str
+			}
+		}
+		if !includeLastSymbol {
+			j--
+		}
+		if i == j {
+			i++
+			j = length - 1
+			str = s[i:]
+			includeLastSymbol = true
+		} else {
+			str = s[i:j]
+			includeLastSymbol = false
+		}
+	}
+
+	return palindromes[maxLength]
+}
+
+func isPalindrome(str string) bool {
+	i, j := 0, len(str)-1
+	for i < j {
+		if str[i] != str[j] {
+			return false
+		}
+		i++
+		j--
+	}
+
+	return true
+}
---
+++ 4-algorithm/longestPalindromicSubstr/longestPalindromicSubstr_test.go
@@ -0,0 +1,29 @@
+package longestPalindromicSubstr_test
+
+import (
+	"algorithm/longestPalindromicSubstr"
+	"testing"
+)
+
+var testCases = []struct {
+	name     string
+	str      string
+	expected string
+}{
+	{"ccd", "ccd", "cc"},
+	{"bb", "bb", "bb"},
+	{"a", "a", "a"},
+	{"cbbd", "cbbd", "bb"},
+	{"babad", "babad", "bab"},
+}
+
+func TestFindLongestPalindrome(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := longestPalindromicSubstr.FindLongestPalindrome(tc.str)
+			if result != tc.expected {
+				t.Errorf("TestCase %v failed. Got %v, expected %v", tc.name, result, tc.expected)
+			}
+		})
+	}
+}
