5775e47483311a28a789dbe761317af5ee3db329
Aleksey Logachev <a.logachev@crpo.su>
2026-06-22 16:50:34 +0300

    1-concurrency Made two goroutines instead of many.

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -8,20 +8,31 @@ import (
 func main() {
 	intCount := 10
 	resultChannel := make(chan int, intCount)
-	for i := 0; i < intCount; i++ {
-		go createTenRandomIntegers(resultChannel)
-	}
+	randomIntegersChan := make(chan int, intCount)
+	go createTenRandomIntegers(resultChannel)
+	go pow2(randomIntegersChan, resultChannel)
 
 	for i := 0; i < intCount; i++ {
 		fmt.Println(<-resultChannel)
 	}
 }
 
-func createTenRandomIntegers(resultChan chan int) {
-	randomNum := rand.IntN(100)
-	go pow2(randomNum, resultChan)
+func createTenRandomIntegers(randomIntegersChan chan int) {
+	defer close(randomIntegersChan)
+	elementsCount := 10
+	randomIntegersSlice := make([]int, elementsCount)
+	for i := 0; i < elementsCount; i++ {
+		randomIntegersSlice[i] = rand.IntN(100)
+	}
+
+	for _, randomInteger := range randomIntegersSlice {
+		randomIntegersChan <- randomInteger
+	}
 }
 
-func pow2(number int, resultChan chan int) {
-	resultChan <- number * number
+func pow2(randomIntegersChan, resultChan chan int) {
+	defer close(resultChan)
+	for number := range randomIntegersChan {
+		resultChan <- number * number
+	}
 }
