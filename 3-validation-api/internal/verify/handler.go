70b9827311670aab046bb29e99826a2a9019d2ab
Aleksey Logachev <a.logachev@crpo.su>
2026-07-26 14:31:30 +0300

    3-validation-api Realized email verification.

--- 3-validation-api/internal/verify/handler.go
+++ 3-validation-api/internal/verify/handler.go
@@ -76,12 +76,42 @@ func (h *VerificationHandler) Send() http.HandlerFunc {
 }
 
 func (h *VerificationHandler) Verify() http.HandlerFunc {
-	return func(w http.ResponseWriter, request *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		payload := VerificationRequest{Hash: r.PathValue("hash")}
+		if err := request.IsValid(payload); err != nil {
+			response.Json(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		db := files.NewJsonDB("database.json")
+		verifications, _ := files.GetCollection[Varification](db, "email_verification")
+
+		index := -1
+		for i, v := range verifications {
+			if v.Hash == payload.Hash {
+				index = i
+				break
+			}
+		}
+
+		if index == -1 {
+			res := VerificationResponse{
+				Success: false,
+				Message: "Could not find an email",
+			}
+			response.Json(w, res, http.StatusNotFound)
+			return
+		}
+
+		verifications = append(verifications[:index], verifications[index+1:]...)
+		if err := files.SetCollection(db, "email_verification", verifications); err != nil {
+			return
+		}
+
 		res := VerificationResponse{
 			Success: true,
 			Message: "The email verified successfully.",
 		}
-		// Тут будет логика подтверждения почты
 		response.Json(w, res, http.StatusOK)
 	}
 }
--- 3-validation-api/internal/verify/payload.go
+++ 3-validation-api/internal/verify/payload.go
@@ -8,6 +8,10 @@ type SendVerificationResponse struct {
 	Success bool `json:"success"`
 }
 
+type VerificationRequest struct {
+	Hash string `json:"hash" validate:"required"`
+}
+
 type VerificationResponse struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
