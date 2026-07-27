8819b22a8c1d1c30bc4622bf2b717cf8e2800382
Aleksey Logachev <a.logachev@crpo.su>
2026-07-27 14:42:12 +0300

    4-order-api Added model.go and migrations.

---
+++ 4-order-api/migrations/auto.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go/order-api/internal/product"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DB_DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&product.Product{})
+}
